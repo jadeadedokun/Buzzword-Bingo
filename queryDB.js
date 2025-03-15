29b80cc69b257977ab761e506786154f1ffa6f2d
irv1 <irv1@pc7-046-l.cs.st-andrews.ac.uk>
2025-03-15 18:32:57 +0000

    merged ines

--- queryDB.js
+++ queryDB.js
@@ -3,24 +3,6 @@ const sqlite3 = require("sqlite3").verbose();
 const scoresDB = new sqlite3.Database("scores.db");
 const wordsDB = new sqlite3.Database("words.db");
 
-const args = process.argv.slice(2);
-
-if (args.length < 1) {
-    process.exit(1);
-}
-
-const queryNumber = parseInt(args[0]);
-
-switch (queryNumber) {
-    case 1:
-        listTopPlayers();
-        break;
-    case 2:
-        listTopWords();
-        break;
-    default:
-        process.exit(1);
-}
 
 function listTopPlayers() {
     scoresDB.all("SELECT player, time FROM scores ORDER BY time ASC LIMIT 5", (err, rows) => {
