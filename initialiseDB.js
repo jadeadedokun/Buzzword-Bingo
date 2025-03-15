4c311a102db36e1a394a94c98f2b8a9fd9350cda
irv1 <irv1@pc7-046-l.cs.st-andrews.ac.uk>
2025-03-15 17:12:42 +0000

    pop database

--- initialiseDB.js
+++ initialiseDB.js
@@ -1,34 +1,21 @@
 const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
 
-// Function to delete and recreate a database
 function initialiseDatabase(dbFile, schemaFile) {
     if (fs.existsSync(dbFile)) {
-        fs.unlinkSync(dbFile); // Delete the existing database file
+        fs.unlinkSync(dbFile);
     }
 
-    const db = new sqlite3.Database(dbFile, (err) => {
-        if (err) {
-            console.error("Error opening database:", err.message);
-        } else {
-            console.log(`${dbFile} created successfully.`);
-        }
-    });
+    const db = new sqlite3.Database(dbFile);
 
-    // Read schema file and execute SQL commands
     const schemaSQL = fs.readFileSync(schemaFile, "utf8");
-    db.exec(schemaSQL, (err) => {
-        if (err) {
-            console.error("Error executing schema:", err.message);
-        } else {
-            console.log(`${dbFile} initialized with tables.`);
-        }
-        db.close();
+
+    db.serialize(() => {
+        db.exec(schemaSQL, () => {
+            db.close();
+        });
     });
 }
 
-// Initialise `scores.db`
 initialiseDatabase("scores.db", "scores.sql");
-
-// Initialise `words.db`
 initialiseDatabase("words.db", "words.sql");
